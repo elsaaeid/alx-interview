19054d56b44b2ca12633a86c5f58db4f7476b45f
elsaaeid <saidsadaoy@gmail.com>
2024-07-25 00:22:47 +0000

    deploy changes

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+validUTF8 function
+"""
+
+
+def validUTF8(data):
+    expected_length = 0
+
+    for num in data:
+        binary_num = bin(num)[2:].zfill(8)
+
+        if expected_length == 0:
+            if binary_num.startswith("0"):
+                continue
+            elif binary_num.startswith("110"):
+                expected_length = 2
+            elif binary_num.startswith("1110"):
+                expected_length = 3
+            elif binary_num.startswith("11110"):
+                expected_length = 4
+            else:
+                return False
+        else:
+            if not binary_num.startswith("10"):
+                return False
+            expected_length -= 1
+
+    return expected_length == 0

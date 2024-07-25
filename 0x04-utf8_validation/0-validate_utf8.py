beccd3a1fb483c061252842ba85d74c3e68a6a00
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-07-25 03:43:05 +0300

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,29 +6,29 @@ validUTF8 function
 
 def validUTF8(data):
     """
-    Returns True if data
+    Function that returns True if data
     is a valid UTF-8 encoding,
     else return False
     """
-    expected_length = 0
+        count = 0
 
-    for num in data:
-        binary_num = bin(num)[2:].zfill(8)
-
-        if expected_length == 0:
-            if binary_num.startswith("0"):
-                continue
-            elif binary_num.startswith("110"):
-                expected_length = 2
-            elif binary_num.startswith("1110"):
-                expected_length = 3
-            elif binary_num.startswith("11110"):
-                expected_length = 4
-            else:
+    for bit in data:
+        binary = bin(bit).replace('0b', '').rjust(8, '0')[-8:]
+        if count == 0:
+            if binary.startswith('110'):
+                count = 1
+            if binary.startswith('1110'):
+                count = 2
+            if binary.startswith('11110'):
+                count = 3
+            if binary.startswith('10'):
                 return False
         else:
-            if not binary_num.startswith("10"):
+            if not binary.startswith('10'):
                 return False
-            expected_length -= 1
+            count -= 1
 
-    return expected_length == 0
+    if count != 0:
+        return False
+
+    return True

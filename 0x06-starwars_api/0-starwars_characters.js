3495a3886df85c101308dc6afe7e2f60041aab0e
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-09 02:18:43 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,7 +3,6 @@
  * Entry point - makes requests to Star Wars API
  * for movie info based movie ID passed as a CLI parameter.
  * Retrieves movie character info then prints their names
- * in order of appearance in the initial response.
  */
 const util = require('util');
 const request = util.promisify(require('request'));
@@ -11,9 +10,9 @@ const movieId = process.argv[2];
 
 async function getMovieCharacters (movieId) {
   const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + filmId;
-  let movieData = await (await request(movieUrl)).body;
-  movieData = JSON.parse(movieData);
-  const characters = movieData.characters;
+  let response = await (await request(movieUrl)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
 
   for (let i = 0; i < characters.length; i++) {
     const urlCharacter = characters[i];

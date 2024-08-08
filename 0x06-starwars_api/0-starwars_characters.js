00d923efc25f3404a22f815ded495653470861d2
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-08 22:10:55 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,3 +6,31 @@
  * in order of appearance in the initial response.
  */
 
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+  } else {
+    const movieData = JSON.parse(body);
+    const characters = movieData.characters;
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error('Error:', error);
+        } else if (response.statusCode !== 200) {
+          console.error('Status:', response.statusCode);
+        } else {
+          const characterData = JSON.parse(body);
+          console.log(characterData.name);
+        }
+      });
+    });
+  }
+});

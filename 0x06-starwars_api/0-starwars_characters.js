f6955307cef3d126ad68604a16e2cc92477940f4
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-09 02:45:44 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -14,7 +14,6 @@ async function getMovieCharacters (movieId) {
   response = JSON.parse(response);
   const characters = response.characters;
 
-
   for (let i = 0; i < characters.length; i++) {
     const urlCharacter = characters[i];
     let character = await (await request(urlCharacter)).body;

16bae672e73dfcc923dd9fa600138f831f5581af
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-08 22:47:24 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,23 +6,20 @@
  * in order of appearance in the initial response.
  */
 
-
-const request = require('request-promise');
+const util = require('util');
+const request = util.promisify(require('request'));
 const movieId = process.argv[2];
 
+async function getMovieCharacters (movieId) {
+  const movieUrl = 'https://swapi-api.alx-tools.com/api/films' + movieId;
+  let response = await (await request(movieUrl)).body;
+  movieData = JSON.parse(response);
+  const characters = movieData.characters;
 
-async function getMovieCharacters(movieId) {
-  try {
-    const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-    const movieData = await request(movieUrl, { json: true });
-    const characters = movieData.characters;
-
-    for (const characterUrl of characters) {
-      const characterData = await request(characterUrl, { json: true });
-      console.log(characterData.name);
-    }
-  } catch (error) {
-    console.error('Error:', error);
+  for (const characterUrl of characters) {
+    const characterData = await request(characterUrl, { json: true });
+    console.log(characterData.name);
   }
 }
+
 getMovieCharacters(movieId);

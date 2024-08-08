ba6e7e987f517c9b5de7bf21ae2c8e244ecd2cf3
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-08 22:19:27 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,31 +6,23 @@
  * in order of appearance in the initial response.
  */
 
-const request = require('request');
+
+const request = require('request-promise');
+
+async function getMovieCharacters(movieId) {
+  try {
+    const movieUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+    const movieData = await request(movieUrl, { json: true });
+    const characters = movieData.characters;
+
+    for (const characterUrl of characters) {
+      const characterData = await request(characterUrl, { json: true });
+      console.log(characterData.name);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
 
 const movieId = process.argv[2];
-
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Status:', response.statusCode);
-  } else {
-    const movieData = JSON.parse(body);
-    const characters = movieData.characters;
-    characters.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          console.error('Error:', error);
-        } else if (response.statusCode !== 200) {
-          console.error('Status:', response.statusCode);
-        } else {
-          const characterData = JSON.parse(body);
-          console.log(characterData.name);
-        }
-      });
-    });
-  }
-});
+getMovieCharacters(movieId);

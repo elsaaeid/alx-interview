a5fe9112c82686b10c6f3937c36d4f4476854cc4
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-22 13:30:23 -0700

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,11 +7,6 @@ Change comes from within
 def makeChange(coins, total):
     """
     Return the minimum number of coins needed to meet a given total
-    Args:
-        coins (list of ints): a list of coins of different values
-        total (int): total value to be met
-    Return:
-        Number of coins or -1 if meeting the total is not possible
     """
     if not coins or coins is None:
         return -1

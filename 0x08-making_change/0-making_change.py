61b838ae5fc035455191f615a601bb16406ed7bf
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2024-08-22 13:17:36 -0700

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -15,9 +15,10 @@ def makeChange(coins, total):
     """
     if not coins or coins is None:
         return -1
+    if total = 0:
+        return 1
     if total <= 0:
         return 0
-
     # Initialize a 1D array to store the minimum coins for each amount
     dp = [float('inf')] * (total + 1)
     dp[0] = 0  # Base case: 0 coins are needed to make 0 total
